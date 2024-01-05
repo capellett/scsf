286be3c2da3b664217a0d25cfd8c61ea710a0f19
laljeet <laljeet@vt.edu>
2024-01-05 13:41:46 -0500

    doc

---
+++ R/data-planning_basins.R
@@ -0,0 +1,5 @@
+#' @title The eight  planning basins in South Carolina
+#' @description This data contains a shapefile for eight planning basins in South Carolina, namely "Broad,"
+#' "Catawba," "Edisto," "Pee Dee," "Upper Savannah," "Saluda," "Lower Savannah-Salkehatchie," and "Santee."
+#' @format A \code{sf and data.frame} with columns for basin name and geometry.
+"planning_basins"
